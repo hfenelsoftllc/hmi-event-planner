dc60c16fa9f0516de61dc43a878bee000a64d7a9
Fenel Hyacinthe <hfenelsoftllc@gmail.com>
2024-10-10 14:15:55 -0400

    Initial commit of the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'hmi-event-planner';
+  title = 'Haitian Music Industry - Event Planner';
 }
